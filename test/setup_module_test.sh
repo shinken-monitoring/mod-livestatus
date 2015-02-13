dde7a4c58a9baffac7ab792f2be7f84a0d43f687
Grégory Starck <gregory.starck@savoirfairelinux.com>
2015-02-13 11:37:48 -0500

    Fix travis

--- test/setup_module_test.sh
+++ test/setup_module_test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -xv
+
 py_version_short=$(python -c "import sys; print(''.join(str(x) for x in sys.version_info[:2]))")
 # -> 26 or 27 or 34 or ..
 
@@ -7,14 +10,23 @@ get_name (){
     echo $(python -c 'import json; print json.load(open("'$1'package.json"))["name"]')
 }
 
+SHI_DST=test/tmp/shinken
+
 setup_submodule (){
+    local dep
+    local mname
+    local mpath
     for dep in $(cat test/dep_modules.txt); do
         mname=$(basename $dep | sed 's/.git//g')
-        git clone $dep ~/$mname
-        ( cd ~/$mname && git status && git log -1)
-        rmname=$(get_name ~/$mname/)
-        cp -r  ~/$mname/module ~/shinken/modules/$rmname
-        [ -f ~/$mname/requirements.txt ] && pip install -r ~/$mname/requirements.txt
+        mpath="test/tmp/$mname"
+        git clone --depth 10 "$dep" "$mpath"
+        ( cd $mpath && git status && git log -1)
+        rmname=$(get_name "$mpath/")
+        ln -s "$PWD/$mpath/module" "$PWD/$SHI_DST/test/modules/$rmname"
+        if [ -f "$mpath/requirements.txt" ]
+        then
+            pip install -r "$mpath/requirements.txt"
+        fi
     done
 }
 
@@ -22,20 +34,44 @@ name=$(get_name)
 
 pip install pycurl
 pip install coveralls
-git clone https://github.com/naparuba/shinken.git ~/shinken
-( cd shinken && git status && git log -1)
-[ -f ~/shinken/test/requirements.txt ] && pip install -r ~/shinken/test/requirements.txt
-[ -f test/dep_modules.txt ] && setup_submodule
-[ -f requirements.txt ] && pip install -r requirements.txt
+
+rm -rf test/tmp
+mkdir -p test/tmp/
+
+git clone --depth 10 https://github.com/naparuba/shinken.git "$SHI_DST"
+( cd "$SHI_DST" && git status && git log -1)
+
 spec_requirement="requirements-${py_version_short}.txt"
-[ -f "$spec_requirement" ] && pip install --use-mirrors -r "$spec_requirement"
+
+(
+    cd "$SHI_DST"
+    pip install -r test/requirements.txt
+    if [ -f "test/${spec_requirement}" ]
+    then
+        pip install -r "test/${spec_requirement}"
+    fi
+)
+
+if [ -f test/dep_modules.txt ]
+then
+    setup_submodule
+fi
+
+if [ -f requirements.txt ]
+then
+    pip install -r requirements.txt
+fi
+if [ -f "$spec_requirement" ]
+then
+    pip install --use-mirrors -r "$spec_requirement"
+fi
+
 test_requirement="test/requirements.txt"
-[ -f "$test_requirement" ] && pip install --use-mirrors -r "$test_requirement"
-rm ~/shinken/test/test_*.py
-cp test/*.py ~/shinken/test/
-[ -d test/etc ] && cp -r test/etc ~/shinken/test/
-cp -r module ~/shinken/modules/$name
-ln -sf ~/shinken/modules ~/shinken/test/modules
-#cd ~/shinken
+if [ -f "$test_requirement" ]
+then
+    pip install --use-mirrors -r "$test_requirement"
+fi
 
+cp -r test/etc "$SHI_DST/test/"
 
+ln -s "$PWD/module" "$SHI_DST/test/modules/$name"
